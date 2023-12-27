84af95080e596b1cb62c6336ecd9f99da6d0f8d9
Alicia Boltryk <boltryk.alicia@gmail.com>
2023-12-27 15:44:04 +0100

    react router mod7p1

---
+++ exercises/mod7/routed-anecdotes/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+import App from './components/App/App'
+import './index.css'
+import { BrowserRouter as Router } from "react-router-dom"
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Router><App /></Router>)
