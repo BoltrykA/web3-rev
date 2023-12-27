6dad0bc70d973c0d72518cc25a582228dedf4e6b
Alicia Boltryk <boltryk.alicia@gmail.com>
2023-12-27 14:37:32 +0100

    tuto mod7

---
+++ exercises/mod7/tutonotes/src/main.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+import ReactDOM from 'react-dom/client'
+import App from './components/App/App.jsx'
+import { BrowserRouter as Router } from "react-router-dom"
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Router><App /></Router>)
